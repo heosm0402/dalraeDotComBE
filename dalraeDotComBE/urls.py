cb874e07106257ca21b04d7efa3d149b525612a8
Heoseongmin <heosm0402@gmail.com>
2022-08-13 12:59:03 +0900

    [Feature] URL for new apps
    Setting url for new apps.

--- dalraeDotComBE/urls.py
+++ dalraeDotComBE/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('studio/', include('studio.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('reply/', include('reply.urls'))
 ]
