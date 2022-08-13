95a4dbe87ffdd90ddc5aa958c9d95af813aa5adc
heoseongmin_mac <heosm0402@gmail.com>
2022-08-13 13:35:28 +0900

    [Feature] New host
    New allowed hosts.

--- dalraeDotComBE/settings.py
+++ dalraeDotComBE/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DalraeDotComDjangoSecretKey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.0.0.138", "localhost"]
+ALLOWED_HOSTS = ["10.0.0.138", "localhost", "106.247.250.251"]
 
 
 # Application definition
