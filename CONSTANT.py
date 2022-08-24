07ffc4aecc170f14f75cb225a09eb294738a850f
Heoseongmin <heosm0402@gmail.com>
2022-08-24 16:48:15 +0900

    [Feature] Constant variable
    Temporary image directory.

--- CONSTANT.py
+++ CONSTANT.py
@@ -5,6 +5,6 @@ import pendulum
 # directory path
 PROJECT_ROOT_DIR = os.path.dirname(__file__)
 LOG_ROOT_DIR = os.path.join(PROJECT_ROOT_DIR, "logs")
-
+IMAGE_FILE_ROOT_DIR = os.path.join("/home/smheo/MinjiDotComDataDir/image")
 # timezone
 KST = pendulum.timezone("Asia/Seoul")
